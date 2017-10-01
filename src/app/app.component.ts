55ef67cc3605af39e7d7dfaa393997e60da98624
jiangxingqi <jxq0816@126.com>
2017-10-01 16:19:45 +0800

    angular get from server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Http, Response, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  data: any;
   title = 'app';
+  constructor(private _http: Http) {
+  }
+  ngOnInit(): void {
+    this.data = this._http.get('de/control/test')
+      .subscribe(data => {
+        this.data = data;
+        console.log(this.data._body);
+      });
+  }
 }
